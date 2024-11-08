7992d61652ae7471ab7d7f76da7e103fbd4770f3
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-11-08 09:41:33 +0530

    changed to Expo

--- App.tsx
+++ App.tsx
@@ -1,7 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
+import { useEffect, useState } from "react";
+
+const loadDatabase = async () => {
+  const dbName = "users.db";
+  const dbAsset = require("./assets/users.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 
 export default function App() {
+  const [dbLoaded, setDbloaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    loadDatabase()
+      .then(() => {
+        setDbloaded(true);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  if (dbLoaded)
+    return (
+      <View>
+        <ActivityIndicator size={"large"} />
+        <Text>Loading...</Text>
+      </View>
+    );
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
